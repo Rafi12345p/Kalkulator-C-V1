500ae953f0adb41e99d1298efdabfda209cc6769
Rafi12345p <muhr89015@gmail.com>
2023-10-27 17:29:33 +0700

    Add files via upload

---
+++ calculator semua hasil.cxx
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int a,b,tambah,kali,kurang,bagi;
+	
+cout<<"=================="<<endl;
+cout<<"= Kalkulator C++ ="<<endl;
+cout<<"=================="<<endl;
+
+cout<<"MASUKKAN ANGKA PERTAMA : ";cin>>a;
+cout<<"MASUKKAN ANGKA KEDUA : ";cin>>b;
+
+tambah = a + b;
+kali = a * b;
+kurang = a - b;
+bagi = a / b;
+
+cout<<"========================="<<endl;
+cout<<"HASIL DARI TAMBAH : "<<tambah<<endl;
+cout<<"HASIL DARI KALI : "<<kali<<endl;
+cout<<"HASIL DARI KURANG : "<<kurang<<endl;
+cout<<"HASIL DARI BAGI : "<<bagi<<endl;
+
+cout<<"========================="<<endl;
+
+cout<<" "<<endl;
+
+cout<<"=================================="<<endl;
+cout<<"= SEMOGA HARIMU MENYENANGKAN ^_^ ="<<endl;
+cout<<"=================================="<<endl;
+
+return 0;
+
+}
